7bd1c014c6e88394b0aae693b1296d525e55f948
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 20:44:04 +0000

    Using modulus operator to check for multiples of 7 and 10

--- loop-control.php
+++ loop-control.php
@@ -4,19 +4,13 @@ $numbers = range(0, 100, 3);
 
 foreach ($numbers as $value) {
 
-	switch($value) {
+	if( $value % 7 === 0 ) {
 
-		case 3:
-			echo "Three";
-			break;
-
-		case 9:
-			echo str_repeat("Nine", 3);
-			break;
-
-		case 15:
-			echo str_repeat("Fifteen", 5);
-			break;
+		echo "First number";
+		echo "Sevens are lucky, this number has " . $value / 7 . " ";
+	}else if( $value % 10 === 0 ) {
+		echo "Last number";
+		echo "{$value} is a round number";
 	}
 
 }
