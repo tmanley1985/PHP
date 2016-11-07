1ddb808d6cd9895fd78ef9d95e7ae89d8895e558
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 21:03:53 +0000

    Refactored using do while loop

--- loop-control.php
+++ loop-control.php
@@ -2,14 +2,19 @@
 
 $numbers = range(0, 100, 3);
 
+$i = 0;
 
-while (list($index, $number) = each ($numbers)) {
-    if( $number % 7 === 0 ) {
+do {
+
+	if( $numbers[$i] % 7 === 0 ) {
 
 		echo "First number";
-		echo "Sevens are lucky, this number has " . $number / 7 . " ";
-	}else if( $number % 10 === 0 ) {
+		echo "Sevens are lucky, this number has " . $numbers[$i] / 7 . " ";
+	}else if( $numbers[$i] % 10 === 0 ) {
 		echo "Last number";
-		echo "{$number} is a round number";
+		echo "{$numbers[$i]} is a round number";
 	}
-}
+
+	$i++;
+	
+} while ($i < count($numbers));
