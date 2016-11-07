bef8c0a4b238a98056997b512ab7715f4cf5461f
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 21:37:14 +0000

    Returning comparison of value and type

--- functions.php
+++ functions.php
@@ -21,7 +21,20 @@ function divide($num1, $num2) {
 	return $num1 / $num2;
 }
 
-echo add($one,$one);
-echo subtract(1, 1);
-echo multiply(1,4);
-echo divide($two, $one);
+// echo add($one,$one);
+// echo subtract(1, 1);
+// echo multiply(1,4);
+// echo divide($two, $one);
+
+function comparison( $val_1, $val_2, $compare_data_types = false ) {
+
+	if( $compare_data_types === false ) {
+		$result = ($val_1 == $val_2) ? "equal" : "not equal";
+		echo "These parameters are " . $result;
+	}else {
+		$result = ($val_1 == $val_2) ? "equal" : "not equal";
+		$datatype = ($val_1 === $val_2) ? "and are of the same datatype" : "and are not of the same datatype";
+		echo "These parameters are " . $result . " " . $datatype;
+	}
+}
+
