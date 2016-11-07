f44da924e5af83c64b4fd79fd3ced9d3a1bb0c68
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 20:31:32 +0000

    Using switch statement in foreach to print strings

--- loop-control.php
+++ loop-control.php
@@ -1,3 +1,22 @@
 <?php
 
 $numbers = range(0, 100, 3);
+
+foreach ($numbers as $value) {
+
+	switch($value) {
+
+		case 3:
+			echo "Three";
+			break;
+
+		case 9:
+			echo str_repeat("Nine", 3);
+			break;
+
+		case 15:
+			echo str_repeat("Fifteen", 5);
+			break;
+	}
+
+}
