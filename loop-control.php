b8875be50893d42f9a323e8b870c9e3867948fa4
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 20:53:48 +0000

    Refactoring using while loop

--- loop-control.php
+++ loop-control.php
@@ -2,13 +2,14 @@
 
 $numbers = range(0, 100, 3);
 
-for ($i=0; $i < count($numbers); $i++) { 
-	if( $numbers[$i] % 7 === 0 ) {
+
+while (list($index, $number) = each ($numbers)) {
+    if( $number % 7 === 0 ) {
 
 		echo "First number";
-		echo "Sevens are lucky, this number has " . $numbers[$i] / 7 . " ";
-	}else if( $numbers[$i] % 10 === 0 ) {
+		echo "Sevens are lucky, this number has " . $number / 7 . " ";
+	}else if( $number % 10 === 0 ) {
 		echo "Last number";
-		echo "{$numbers[$i]} is a round number";
+		echo "{$number} is a round number";
 	}
 }
