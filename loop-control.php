69dc41d463aa4658f450251cf8fd62e5bed630ca
tmanley1985 <tmanley1985@gmail.com>
2016-11-07 20:48:44 +0000

    Using a for loop instead of a foreach loop

--- loop-control.php
+++ loop-control.php
@@ -2,15 +2,13 @@
 
 $numbers = range(0, 100, 3);
 
-foreach ($numbers as $value) {
-
-	if( $value % 7 === 0 ) {
+for ($i=0; $i < count($numbers); $i++) { 
+	if( $numbers[$i] % 7 === 0 ) {
 
 		echo "First number";
-		echo "Sevens are lucky, this number has " . $value / 7 . " ";
-	}else if( $value % 10 === 0 ) {
+		echo "Sevens are lucky, this number has " . $numbers[$i] / 7 . " ";
+	}else if( $numbers[$i] % 10 === 0 ) {
 		echo "Last number";
-		echo "{$value} is a round number";
+		echo "{$numbers[$i]} is a round number";
 	}
-
 }
