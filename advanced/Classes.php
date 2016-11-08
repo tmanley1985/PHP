2adb34aa7482d400b5ab23d7f255293b55596ea3
tmanley1985 <tmanley1985@gmail.com>
2016-11-08 15:10:35 +0000

    Added class hierarchy

---
+++ advanced/Classes.php
@@ -0,0 +1,29 @@
+<?php
+
+class Computer {
+
+}
+
+class Workstation extends Computer {
+
+}
+
+class Mac extends Workstation {
+
+}
+
+class PC extends Workstation {
+
+}
+
+class Server extends Computer {
+
+}
+
+class WebServer extends Server {
+
+}
+
+class DatabaseServer extends Server {
+
+}
