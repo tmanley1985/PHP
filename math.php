82be507877e08c9b9ebebd551a159f2669d54f20
tmanley1985 <tmanley1985@gmail.com>
2016-11-08 14:15:13 +0000

    Refactored the math functions to use a variable number of arguments

--- math.php
+++ math.php
@@ -2,20 +2,65 @@
 
 class Math {
 	
-	public function add($num1, $num2) {
-		return $num1 + $num2;
+	public function add($num1, $num2, ...$nums) {
+		
+		if(count($nums) === 0) {
+			
+			return $num1 + $num2;
+		}else {
+			$numbers = array_merge([$num1, $num2], $nums);
+
+			return array_reduce($numbers, function($val1, $val2) {
+				return $val1 + $val2;
+			});
+		}
+		
 	}
 
-	public function subtract($num1, $num2) {
-		return $num1 - $num2;
+	public function subtract($num1, $num2, ...$nums) {
+		if(count($nums) === 0) {
+			return $num1 - $num2;
+		}else {
+			$numbers = array_merge([$num2], $nums);
+
+			return array_reduce($numbers, function($val1, $val2){
+				
+				return $val1 - $val2;
+
+			}, $num1);
+
+		}
+		
 	}
 
-	public function multiply($num1, $num2) {
-		return $num1 * $num2;
+	public function multiply($num1, $num2, ...$nums) {
+		if(count($nums) === 0) {
+			return $num1 * $num2;
+		}else {
+
+			$numbers = array_merge([$num2], $nums);
+
+			return array_reduce($numbers, function($val1, $val2){
+
+				return $val1 * $val2;
+			}, $num1);
+		}
+
+
 	}
 
-	public function divide($num1, $num2) {
-		return $num1 / $num2;
+	public function divide($num1, $num2, ...$nums) {
+		if(count($nums) === 0) {
+			return $num1 / $num2;
+		}else {
+
+			$numbers = array_merge([$num2], $nums);
+
+			return array_reduce($numbers, function($val1, $val2){
+				return $val1 / $val2;
+			}, $num1);
+
+		}
 	}
 
 }
